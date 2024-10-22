553d172fd98f59e7bd0caf39b9652d54016d305e
miguellmoura <miguelmouracvm23@gmail.com>
2024-10-22 18:23:20 -0300

    testE

--- src/main/kotlin/br/pucpr/authserver/departments/responses/DepartmentResponse.kt
+++ src/main/kotlin/br/pucpr/authserver/departments/responses/DepartmentResponse.kt
@@ -3,7 +3,7 @@ package br.pucpr.authserver.departments.responses
 import br.pucpr.authserver.departments.Department
 
 data class DepartmentResponse(val id: Long, val name: String, val description: String, val officeRoutine: String) {
-    constructor(department: Department): this(
+    constructor(department: Department): this (
         id = department.id!!,
         name = department.name,
         description = department.description,
