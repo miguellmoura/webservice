effb938ca5e073debbb51afb6fbf305f2e2b6358
miguellmoura <miguelmouracvm23@gmail.com>
2024-11-17 16:02:43 -0300

    remove unused imports and functions

--- src/main/kotlin/br/pucpr/authserver/schedules/ScheduleService.kt
+++ src/main/kotlin/br/pucpr/authserver/schedules/ScheduleService.kt
@@ -2,17 +2,11 @@ package br.pucpr.authserver.schedules
 
 import br.pucpr.authserver.schedules.requests.CreateScheduleRequest
 import br.pucpr.authserver.users.SortDir
-import br.pucpr.authserver.users.User
 import org.springframework.data.domain.Sort
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class ScheduleService(val repository: ScheduleRepository) {
-
-    fun findByIdOrNull(id: Long): List<Schedule>? {
-        return repository.findByIdCourt(id)
-    }
 
     fun getScheduleById(id: Long): Schedule {
         return repository.findById(id).orElseThrow { NoSuchElementException("Schedule not found with id: $id") }
